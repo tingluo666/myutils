cd4860ffbcf9286068d62da334e04a387f68e2ca
万博 <wanbo@juxiangfen.com>
2019-03-22 10:53:28 +0800

    init

---
+++ src/main/java/com/example/myutils/MyutilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.myutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyutilsApplication.class, args);
+    }
+
+}
